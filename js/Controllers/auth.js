f23d73ff8c17b3aff90df593e19cad78ed4ba88b
aboukhri <bks.ayb@gmail.com>
2020-06-29 20:00:29 +0100

    save user in cookies

--- js/Controllers/auth.js
+++ js/Controllers/auth.js
@@ -3,8 +3,19 @@
 
 var accountsRef = firebase.database().ref('accounts');
 
-app.controller("loginController", function($scope){   
+app.controller("loginController", ["$scope", "$cookies", function($scope, $cookies){   
+        var accountsList = [];
+
+        accountsRef.on('value', function(accounts){
+            accountsList = [];
+            Object.keys(accounts.val()).forEach(function (key) {
+                accountsList.push({"key": key, "info": accounts.val()[key]});
+            });
+        }, function(error){
+            console.log("error (accounts): " + error.code);
+        });
         $scope.logIn = function(){
+            console.log(accountsList);
             var username = document.getElementById('username');
             var password = document.getElementById('password');
             var logged = false;
@@ -12,19 +23,17 @@ app.controller("loginController", function($scope){
                 $scope.message = "must fill both fields";
                 return;
             }
-            accountsRef.on('value', function(accounts){
-                accounts.forEach(function (elem) {
-                    if (elem.val().username == username.value && elem.val().password == password.value){
+            if (accountsList.length > 0){
+                for(i=0;i<accountsList.length;i++){
+                    if (accountsList[i].info.username == username.value && accountsList[i].info.password == password.value){
                         logged = true;
-                        window.location.href = "#!admin";
+                        $cookies.put("idUser", accountsList[i].key);
+                        window.location.href = "#/admin";
+                        return;
                     }
-                });
-            }, function(error){
-                console.log("error (accounts): " + error.code);
-            });
-            if (!logged){
-                $scope.message = "username or password is incorrect";
-                password.value = "";
+                }
             }
+            $scope.message = "username or password is incorrect";
+            password.value = "";
         }
-});
+}]);
--- js/app.js
+++ js/app.js
@@ -1,32 +1,38 @@
-var app=angular.module("grootsApp",["ngRoute", "angularCSS"]);
+var app=angular.module("grootsApp",["ngRoute", "angularCSS", "ngCookies"]);
 
 app.config(["$routeProvider", "$locationProvider",function($routeProvider, $locationProvider){
 	$routeProvider
 	.when("/",{
 		templateUrl: "view/home.html",
 		controller: "langCtrl",
-		css: "../css/home.css"
+		css: "css/home.css"
     })
     /*.when("/About",{
         templateUrl: "view/about.html",
       //  controller: "AdminController"
     })*/.when("/login",{
+		url: "/login",
     	templateUrl: "view/auth.html",
-        controller: "loginController"
+		controller: "loginController",
+		css: "css/auth.css"
 	}).when("/admin",{
     	templateUrl: "view/customers_list.html",
-        controller: "customerListController"
+		controller: "customerListController",
+		css: "css/list.css"
 	}).otherwise({
         redirectTo: "/"
 	});
-	$locationProvider.html5Mode(true);
+	$locationProvider.hashPrefix('');
+	//$locationProvider.html5Mode({
+	//	enabled: true,
+	//	requireBase: false
+	  //});
 }]);
-
 
 app.controller("langCtrl",["$scope","$http",function($scope,$http){
 	$scope.lang=null;
 	$scope.selectedLang="fr";
-	
+
 	$http.get('./language.json',{header : {'Content-Type' : 'application/json'}})
 	.then(function(res){
         $scope.lang=res.data["fr"];
